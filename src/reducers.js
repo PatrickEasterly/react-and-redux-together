a82f997ead4d4055d20f8413f170a0ef0e7fb054
Patrick <pateast678@gmail.com>
2020-02-12 10:40:14 -0500

    adds actions and reducers for counter; installs react-redux

---
+++ src/reducers.js
@@ -0,0 +1,29 @@
+import { 
+    INCREMENT, 
+    DECREMENT, 
+    RESET
+} from "./actions";
+
+
+// Our ideal/default state is just a number:
+// 0
+// 0 to 1
+
+
+export function count(state, action) {
+    let newState = state;
+    switch(action.type) {
+        case INCREMENT:
+            newState += 1;
+            break;
+        case DECREMENT:
+            newState -= 1;
+            break;
+        case RESET:
+            newState = 0;
+            break;
+        default:
+            break;
+    } 
+    return newState;
+}
